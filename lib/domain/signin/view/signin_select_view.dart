307d71a74d5c7e4b6c55776ee2c05db970d3fc81
Drywoo <maxgun4757@naver.com>
2024-04-22 20:49:22 +0900

    Add: 로그인 선택창 구현

--- lib/domain/signin/view/signin_select_view.dart
+++ lib/domain/signin/view/signin_select_view.dart
@@ -1,34 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:lift/View/widget/resource/reusable_button.dart';
+import 'package:lift/domain/signin/view/widget/reusable_image_login_button.dart';
 
 class SignInSelectView extends StatelessWidget {
   const SignInSelectView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: SingleChildScrollView(
         child: SafeArea(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-
+            const SizedBox(height: 40),
+            _SelectLoginText(),
+            const SizedBox(height: 10),
+            _SelectLoginSubText(),
+            const SizedBox(height: 50),
+            _EmailLoginButton(),
+            const SizedBox(height: 10),
+            _GoogleLoginButton(),
+            const SizedBox(height: 10),
+            _AppleLoginButton(),
           ],
         )),
       ),
     );
   }
 
-  Widget SelectLoginText() {
+  Widget _SelectLoginText() {
     return Container(
     width: double.infinity,
     padding: const EdgeInsets.only(left: 20),
     child: const Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Text('안녕하세요\nLIFT입나다',
+        Text('안녕하세요\nLIFT입니다',
         style: TextStyle(
-        fontWeight: FontWeight.bold,
-        fontFamily: 'NanumGothicBold',
+        fontWeight: FontWeight.w600,
+        fontFamily: 'NanumGothic',
         fontSize: 40),
         textAlign: TextAlign.left),
       ],
@@ -36,12 +48,69 @@ class SignInSelectView extends StatelessWidget {
   );
 }
 
-  Widget SelectLoginSubText() {
+Widget _SelectLoginSubText() {
     return Container(
-     width: double.infinity,
-     padding: EdgeInsets.only(left: 20,right: 20),
-     
+      padding: const EdgeInsets.only(left: 20),
+      child: const Opacity(
+        opacity: 0.2,
+        child: Text("회원 서비스 사용을 위해 로그인해주세요",
+            style: TextStyle(fontFamily: 'NanumGothic', fontSize: 15),
+            textAlign: TextAlign.left,
+        ),
+      ),
     );
   }
 }
 
+Widget _EmailLoginButton() {
+  return Container(
+    padding: const EdgeInsets.only(left: 20,right: 20),
+    child: ReusableButton(
+    width: double.infinity,
+    height: 40,
+    onPressed: () async {
+      print('안농');
+    },
+    style: TextButton.styleFrom(
+      side: const BorderSide(color: Colors.black, width: 1.5),
+      backgroundColor: const Color(0xff4F60FE),
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(
+          Radius.circular(10),
+        ),
+      ),
+    ),
+    text: '이메일 로그인',
+    textStyle: const TextStyle(color: Colors.white,fontWeight: FontWeight.bold),
+  ),
+  );
+}
+
+
+Widget _AppleLoginButton() {
+  return Container(
+    padding: EdgeInsets.only(left: 20, right: 20),
+    height: 40,
+    child: ReusableImageLoginButton(
+      imagePath: 'assets/images/remove_background_Login_apple.png',
+      text: '애플 로그인',
+      onPressed: () {
+        print('안녕');
+      },
+    ),
+  );
+}
+
+Widget _GoogleLoginButton() {
+  return Container(
+    height: 40,
+    padding: const EdgeInsets.only(left: 20, right: 20),
+    child: ReusableImageLoginButton(
+      imagePath: 'assets/images/remove_background_Login_google.png',
+      text: '구글 로그인',
+      onPressed: () {
+        print('안녕');
+      },
+    ),
+  );
+}
--- lib/domain/splash/view_models/service/splash_service.dart
+++ lib/domain/splash/view_models/service/splash_service.dart
@@ -1,10 +1,11 @@
 import 'dart:async';
 import 'package:get/get.dart';
 import 'package:lift/domain/login/view/login_view.dart';
+import 'package:lift/domain/signin/view/signin_select_view.dart';
 
 class SplashService {
   void isLogin() {
-    Timer(const Duration(seconds: 2), () => Get.offAll(const LoginView()));
+    Timer(const Duration(seconds: 2), () => Get.offAll(const SignInSelectView()));
 
   }
 }
